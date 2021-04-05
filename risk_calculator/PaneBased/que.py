2aab5fe5b7c9e87dd08c91f38817cc0132d0cd83
Ramesh <morjariaramesh@gmail.com>
2021-04-05 16:17:40 +1000

    pane based risk calculator first version

---
+++ risk_calculator/PaneBased/que.py
@@ -0,0 +1,70 @@
+q1 = {'question':"1. What is your current age?", 
+    'choices' : [
+      ('0', 'More than 60'),
+      ('2', 'Between 51 and 60'), 
+      ('5', 'Between 41 and 50'), 
+      ('8', 'Between 31 and 40'), 
+      ('10', '30 or younger')]}
+q2 = {'question':"2. Please estimate when you will need to withdraw 20% of your current portfolio value, such as a need for a house down payment or some other major financial need?", 'choices' : [
+  ('0', 'Within the next year'), 
+  ('2', '2 - 5 years from now'), 
+  ('5', '5 - 10 years from now'), 
+  ('8', '10 - 20 years from now'), 
+  ('10', 'More than 20 years from now')]}
+q3 = {'question':"3. If you were to lose your job today, how long will you be able to maintain your current spending life style before you run out of money in your Savings and Checking account?", 
+'choices' : [
+  ('0', '1 week'), 
+  ('2', '1 month'), 
+  ('5', '3 months'), 
+  ('8', '6 months or more'), 
+  ('10', 'I am retired')]}
+q4 = {'question':"4. What is your total annual income? ( Work, Pension, Social Security, etc.)", 'choices' : [
+  ('0', 'Less than $50,000'), 
+  ('2', '$50,000 - $100,000'), 
+  ('5', '$100,000 - $150,000'), 
+  ('8', '$150,000 - $250,000'), 
+  ('10', 'More than $250,000')]}
+q5 = {'question':"5. Please rate the stability of your income.", 
+'choices' : [
+  ('0', 'Very low'), 
+  ('2', 'Below average'), 
+  ('5', 'Average'), 
+  ('8', 'Above Average'), 
+  ('10', 'Very High')]}
+q6 = {'question':"6. If you unexpectedly received $20,000 to invest today, what would you do?", 
+'choices' : [
+  ('0', 'Deposit it in bank account, money market account or an insured cd'), 
+  ('2', 'Invest only in safe quality bonds or bond funds'), 
+  ('5', 'Invest in a proper mix of bonds and stocks'), 
+  ('8', 'Invest only in stocks or stock funds'), 
+  ('10', 'Buy $20,000 worth of lottery tickets')]}
+q7 = {'question':"7. When you think of the word “risk” in a financial context, which of the following words come to mind?", 
+'choices' : [
+  ('0', 'Absolute loss'), 
+  ('2', 'Danger'), 
+  ('5', 'Uncertainty'), 
+  ('8', 'Opportunity'), 
+  ('10', 'Thrill')]}
+q8 = {'question':"8. Which of these statements would best describe your attitudes about the next three years performance of this investment?", 
+'choices' : [
+  ('0', 'I need to see at least a little return'), 
+  ('2', "I'd have a hard time tolerating any losses"), 
+  ('5', 'I can tolerate a small loss'), 
+  ('8', 'I can tolerate a loss'), 
+  ('10', "I don't mind a loss")]}   
+q9 = {'question':"9. Throughout the 17-month bear during the 2008 Mortgage Crisis, the S&P 500 lost 50% of its value. Which of the following would you have done if your portfolio experiences a 50% loss?", 
+'choices' : [
+  ('0', 'Sell all of your remaining investments and remain in cash'), 
+  ('2', "Move all of your investments into bonds and CDs"), 
+  ('5', 'Change to a more conservative allocation'), 
+  ('8', 'Hold on to your investments'), 
+  ('10', "Invest more")]} 
+q10 = {'question':"10. How emotionally comfortable are you with volatility?", 
+'choices' : [
+  ('0', 'Plan A'), 
+  ('2', "Plan B"), 
+  ('5', 'Plan C'), 
+  ('8', 'Plan D'), 
+  ('10', "Plan E")]}
+
+questions = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10]
